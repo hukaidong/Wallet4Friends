0367c28d9c6f3a495b5ceaa4e61f9c8a5ac50ed8
Kaidong Hu <dongforregister@gmail.com>
2021-09-05 17:19:52 -0400

    status update

--- app/models/money_transfer.rb
+++ app/models/money_transfer.rb
@@ -1,4 +1,5 @@
 class MoneyTransfer < ApplicationRecord
+  enum complete_status: [:completed, :incompleted], _default: :incompleted
   belongs_to :sender, class_name: 'User', foreign_key: 'sender_id'
   belongs_to :receiver, class_name: 'User'
 end
--- app/views/money_transfers/_money_transfer.json.jbuilder
+++ app/views/money_transfers/_money_transfer.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! money_transfer, :id, :memo, :amount, :created_at, :updated_at
+json.extract! money_transfer, :id, :complete_status, :memo, :amount, :created_at, :updated_at
 json.url money_transfer_url(money_transfer, format: :json)
